a7a0b74f9f54dab52bb94a8242ab7d0ff958fdb8
richbruton <richbruton@gmail.com>
2022-05-04 11:04:30 +0000

    fix a typo

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def about():
 def about_member(member_name):
     member = {}
     with open("data/company.json", "r") as json_data:
-        data =json.load(json_data)
+        data = json.load(json_data)
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
